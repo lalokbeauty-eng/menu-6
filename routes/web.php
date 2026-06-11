1f46e856ff99649e2aa200776227a45c6c876116
aarfigo <aarfigo44@gmail.com>
2026-06-11 16:04:20 -0400

    menu 66

---
+++ routes/web.php
@@ -0,0 +1,18 @@
+<?php
+
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\DeliveryZoneController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', [ProductController::class, 'menu'])->name('menu');
+Route::get('/api/delivery-zones', [DeliveryZoneController::class, 'getActive'])->name('api.zones.active');
+
+Route::prefix('admin')->name('admin.')->group(function () {
+    Route::get('/', [ProductController::class, 'index'])->name('products.index');
+    Route::post('/products', [ProductController::class, 'store'])->name('products.store');
+    Route::put('/products/{product}', [ProductController::class, 'update'])->name('products.update');
+    Route::delete('/products/{product}', [ProductController::class, 'destroy'])->name('products.destroy');
+    
+    // Rutas de Zonas de Entrega
+    Route::resource('zones', DeliveryZoneController::class);
+});
